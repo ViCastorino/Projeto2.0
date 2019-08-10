7e56ae1be67f6d0c5fda2457c4595c205b8d0979
BabiMoreira <babisantos631@gmail.com>
2019-08-10 14:58:33 -0300

    linkar o script com a page de cadastro

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -1,37 +1,26 @@
+// let btnpaciente = document.getElementById('first');
+// let btnorganization = document.getElementById('second');
+// let a = document.getElementById('formulários');
+// let treze = document.getElementById('buttons');
+// let x = document.getElementById('paciente');
+// let y = document.getElementById('instituicao');
 
-let btnpaciente = document.getElementById('first');
-let btnorganization = document.getElementById('second');
-let a = document.getElementById('maior');
-let treze = document.getElementById('treze');
-let p = document.getElementById('5');
-let x = document.getElementById('form.a');
-let y = document.getElementById('form.b');
 
-btnpaciente.addEventListener('click', function(){
-    a.style.display =  'block';
-    treze.style.display = 'none';
-    x.style.display =  'block';
-    y.style.display =  'none';
-    // p.style.display =  'none';
-    console.log('clicou1');
-});
+// btnpaciente.addEventListener('click', function(){
+//     a.style.display =  'block';
+//     treze.style.display = 'none';
+//     x.style.display =  'block';
+//     y.style.display =  'none';
+//     console.log('clicou1');
+// });
 
-btnorganization.addEventListener('click', function(){
-    a.style.display = 'block';
-    x.style.display =  'none';
-    y.style.display =  'block';
+// btnorganization.addEventListener('click', function(){
+//     a.style.display = 'block';
+//     x.style.display =  'none';
+//     y.style.display =  'block';
     
-    // p.style.display =  'none';
-    treze.style.display = 'none';
-    console.log('clicou2');
-});
+//     // p.style.display =  'none';
+//     treze.style.display = 'none';
+//     console.log('clicou2');
+// });
 
-
-// function inicaLogin(loginID) {
-//     const login = document.getElementById(loginID);
-//     login.classList.add('mostrar');
-// }
-// const entre =document.querySelector('.secondary');
-// entre.addEventListener('click',function(){
-//     inicaLogin('login');
-// } );
