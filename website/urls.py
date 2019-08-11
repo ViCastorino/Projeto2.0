6f9fb674d7a4df7d898433ef8c298df331fc9d43
pedro <52453820+pedrosvg@users.noreply.github.com>
2019-08-11 01:20:07 -0300

    add url /agendar para ver resultados da pesquisa

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('login', views.login),
     path('cadastro', views.cadastro),
     path('cadastro_instituicao', views.cadastro_instituicao),
-    
-    # Comentei pq quando eu fui rodar o projeto, a função ainda não tinha sido postada no repositório, então causava erro.
-    # path('cadastro_cliente', views.cadastro_cliente)
+    path('agendar', views.agendar),
+
 ]
