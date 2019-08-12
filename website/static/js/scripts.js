b808d7f7612a47bdf96d59cb7efea25725fb06ec
ViCastorino <vih.teodoro16@gmail.com>
2019-08-12 01:27:44 -0300

    Cadastro quase pronto

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -1,28 +1,26 @@
-// let btnpaciente = document.getElementById('first');
-// let btnorganization = document.getElementById('second');
-// let a = document.getElementById('formulários');
-// let treze = document.getElementById('buttons');
-// let x = document.getElementById('paciente');
-// let y = document.getElementById('instituicao');
+let btnpaciente = document.getElementById('first');
+let btnorganization = document.getElementById('second');
+let container = document.getElementById('container');
+let x = document.getElementById('paciente');
+let y = document.getElementById('instituicao');
+let a = document.getElementById('formulários');
 
 
-// btnpaciente.addEventListener('click', function(){
-//     a.style.display =  'block';
-//     treze.style.display = 'none';
-//     x.style.display =  'block';
-//     y.style.display =  'none';
-//     console.log('clicou1');
-// });
+btnpaciente.addEventListener('click', function(){
+    a.style.display =  'block';
+    container.style.display = 'none';
+    x.style.display =  'block';
+    y.style.display =  'none';
+    console.log('clicou1');
+});
 
-// btnorganization.addEventListener('click', function(){
-//     a.style.display = 'block';
-//     x.style.display =  'none';
-//     y.style.display =  'block';
-    
-//     // p.style.display =  'none';
-//     treze.style.display = 'none';
-//     console.log('clicou2');
-// });
+btnorganization.addEventListener('click', function(){
+    a.style.display = 'block';
+    x.style.display =  'none';
+    y.style.display =  'block';
+    container.style.display = 'none';
+    console.log('clicou2');
+});
 
 
 
