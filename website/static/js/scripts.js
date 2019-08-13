ada1796fdf0ea5a9bc2de166d2699481ca800039
ViCastorino <vih.teodoro16@gmail.com>
2019-08-13 01:35:01 -0300

    Arrumando tela cadastro

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -5,19 +5,21 @@ let x = document.getElementById('paciente');
 let y = document.getElementById('instituicao');
 let a = document.getElementById('formulários');
 
+x.style.display =  'none';
+y.style.display =  'none';
 
 btnpaciente.addEventListener('click', function(){
-    a.style.display =  'block';
+    a.style.display =  'flex';
     container.style.display = 'none';
-    x.style.display =  'block';
+    x.style.display =  'flex';
     y.style.display =  'none';
     console.log('clicou1');
 });
 
 btnorganization.addEventListener('click', function(){
-    a.style.display = 'block';
+    a.style.display = 'flex';
     x.style.display =  'none';
-    y.style.display =  'block';
+    y.style.display =  'flex';
     container.style.display = 'none';
     console.log('clicou2');
 });
